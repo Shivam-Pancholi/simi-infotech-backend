faa5498b9311ac044e1d8e7340046cf36b4d7c21
Accessai <accessai@Accessais-MacBook-Air.local>
2022-08-11 23:16:25 +0530

    updated settings

--- siminfotech/settings.py
+++ siminfotech/settings.py
@@ -152,6 +152,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_URL = 'static/'
+
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
