5d4be35ff984c002786b35df9b5871547c2fa450
Hritik05 <50368338+Hritik05@users.noreply.github.com>
2022-08-23 22:01:19 +0530

    db changes

--- siminfotech/settings.py
+++ siminfotech/settings.py
@@ -113,8 +113,7 @@ WSGI_APPLICATION = 'siminfotech.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/mysite.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -165,3 +164,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
