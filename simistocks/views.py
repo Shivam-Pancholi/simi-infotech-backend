621f12ce6b53fc3e27bedf33fd003316f11ae4f1
Hritik05 <hritikzelawat@gmail.com>
2022-09-12 09:06:14 +0530

    Sorted data date wise

--- simistocks/views.py
+++ simistocks/views.py
@@ -211,10 +211,8 @@ def simi_whatsapp(request):
     token = user.whatsapp_token
     url = "https://graph.facebook.com/v13.0/%s/messages" % phone_id
     limit = user.msg_limit
-    if limit < request.data.get("phone_numbers"):
+    if limit < len(request.data.get("phone_numbers")):
         return Response("Sorry only %s msg is remaining" % limit)
-    print(request.data)
-    print(request.data.get("phone_numbers"))
     if request.data.get("image") or request.data.get("video") or request.data.get("document"):
         user = Userdata.objects.filter(user__id=request.user.id).last()
         print(user.template_img)
