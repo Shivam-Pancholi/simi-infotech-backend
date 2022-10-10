e68aa74228ab005384b56c572666cd80bb850729
Hritik05 <hritikzelawat@gmail.com>
2022-10-10 08:51:13 +0530

    NEW API ADDED

--- simistocks/urls.py
+++ simistocks/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from simistocks.views import simidata, list_users, update_user, simi_whatsapp, templates, send_wp_msg
+from simistocks.views import simidata, list_users, update_user, simi_whatsapp, templates, send_wp_msg, delete_data, \
+    exchane_wp_msg
 
 urlpatterns = [
     path('simidata', simidata),
@@ -8,5 +9,7 @@ urlpatterns = [
     path('update_user', update_user),
     path('simi_whatsapp', simi_whatsapp),
     path('templates', templates),
-    path('send_wp_msg', send_wp_msg)
+    path('send_wp_msg', send_wp_msg),
+    path('delete_data', delete_data),
+    path('exchane_wp_msg', exchane_wp_msg)
 ]
