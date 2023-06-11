0646a5cf36a5c50b4b484444f3db3f99d95eaa0a
Hritik05 <hritikzelawat@gmail.com>
2023-06-11 12:56:02 +0530

    simistocksandscheme api created

--- simistocks/models.py
+++ simistocks/models.py
@@ -9,7 +9,7 @@ class Userdata(models.Model):
     whatsapp_account_id = models.CharField(max_length=2048, null=True, blank=True)
     whatsapp_phone_no_id = models.CharField(max_length=2048, null=True, blank=True)
     whatsapp_token = models.CharField(max_length=4096, null=True, blank=True)
-    templates = models.JSONField(default=list)
+    templates = models.JSONField(default=dict)
     template_img = models.FileField(upload_to='documents', blank=True, null=True)
     msg_limit = models.IntegerField(default=0)
     scheme_file_name = models.TextField(null=True, blank=True)
