d294fb0d9c19b2122f2d489a40b22421a9c26664
Hritik05 <hritikzelawat@gmail.com>
2023-11-05 22:54:56 +0530

    commenting temporary service up shifting simiinfotech message to kapil

--- simistocks/views.py
+++ simistocks/views.py
@@ -373,12 +373,12 @@ def send_wp_msg(request):
     # }
     # response = requests.request("POST", url, headers=headers, data=payload).json()
     # print(response)
-    if request.query_params.get('token') == "107427908838031" and request.query_params.get('username') == "simiinfotech":
-        user = Userdata.objects.filter(user__username="kapil",
-                                       whatsapp_phone_no_id="111935795037601").last()
-    else:
-        user = Userdata.objects.filter(user__username=request.query_params.get('username'),
-                                       whatsapp_phone_no_id=request.query_params.get('token')).last()
+    # if request.query_params.get('token') == "107427908838031" and request.query_params.get('username') == "simiinfotech":
+    #     user = Userdata.objects.filter(user__username="kapil",
+    #                                    whatsapp_phone_no_id="111935795037601").last()
+    # else:
+    user = Userdata.objects.filter(user__username=request.query_params.get('username'),
+                                   whatsapp_phone_no_id=request.query_params.get('token')).last()
     if not user:
         return Response('Invalid Credentials')
     number_list = request.query_params.get('receiverMobileNo').split(',')
