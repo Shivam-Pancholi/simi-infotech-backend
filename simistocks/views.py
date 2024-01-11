30d31063c0a88dc4523e25a275ddb24ab5ab644c
Hritik05 <hritikzelawat@gmail.com>
2024-01-11 20:48:28 +0530

    block number field added

--- simistocks/views.py
+++ simistocks/views.py
@@ -305,7 +305,7 @@ def simi_whatsapp(request):
         else:
             text = ""
     for numbers in ast.literal_eval(request.data.get("phone_numbers")):
-        if numbers in user.blocked_number:
+        if int(numbers) in user.blocked_number:
             continue
         if data.get("name") in ["only_text", "text_with_image", "text_button_image"]:
             if data.get("name") == "only_text":
@@ -394,7 +394,7 @@ def send_wp_msg(request):
         return Response('Invalid Credentials')
     number_list = request.query_params.get('receiverMobileNo').split(',')
     for number in number_list:
-        if number in user.blocked_number:
+        if int(number) in user.blocked_number:
             continue
         if request.query_params.get('fileurl'):
             a = urlparse(request.query_params.get('fileurl'))
@@ -493,7 +493,7 @@ def exchange_wp_msg(request):
                 template = {"name": "%s" % data.get("name"), "language": {"code": "%s" % data.get("language")}}
     for users in ast.literal_eval(request.data.get("data")):
         numbers = users.get("K5")
-        if numbers in user.blocked_number:
+        if int(numbers) in user.blocked_number:
             continue
         if msg.find("{{name}}") >= 0:
             msg = msg.replace("{{name}}", users.get("K4"))
