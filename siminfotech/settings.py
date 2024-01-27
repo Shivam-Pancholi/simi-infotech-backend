5e61cb5711e1f1f130c5e7299e2232136afeb81e
Hritik05 <hritikzelawat@gmail.com>
2024-01-27 10:50:39 +0530

    css issue fix

--- siminfotech/settings.py
+++ siminfotech/settings.py
@@ -156,11 +156,8 @@ USE_TZ = True
 
 # PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
-MEDIA_ROOT = '/media'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
